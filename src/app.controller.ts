87bd53363fe831781735f4f47dc822222177efe7
Python-Is-Long <seancg68@gmail.com>
2024-03-28 09:33:14 -0500

    handlebars demo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
-  @Render('index')
-  root() {
-    return { message: 'Hello world!' };
+  @Render('index') // Render the 'index.hbs' template
+  home() {
+    return {
+      title: 'Use HTML Template Engine', // Dynamic data to be passed to the template
+      steps: ['1. Download HTML Template Engine', '2. ???', '3. Profit!'] // Dynamic data to be passed to the template
+    };
   }
 }
